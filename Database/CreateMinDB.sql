57b1154ead2226c01ff616ecd88047cc510e45cb
Виктория <vikaveris@ya.ru>
2018-09-21 16:26:25 +0300

    Update plan, file with actions on SPA, schema and sql file for minimal database

--- Database/CreateMinDB.sql
+++ Database/CreateMinDB.sql
@@ -13,16 +13,9 @@ use MinOnlineStore
 create table Products 
 (
 	ID_product bigint not null identity primary key,
-	ID_category bigint not null,
 	PName nvarchar(50) not null,
 	Amount decimal(3,0) not null,
 	Cost money not null
-);
-create table Categories
-(
-	ID_category bigint not null identity primary key,
-	ID_subcategory bigint null,
-	CName nvarchar(50) not null
 );
 create table Items_of_orders
 (
@@ -53,27 +46,9 @@ create table Users
 );
 
 -- Foreign keys
-alter table Products
-add foreign key (ID_category) references Categories(ID_category)
-alter table Categories
-add foreign key (ID_category) references Categories(ID_category)
 alter table Items_of_orders
 add foreign key (ID_product) references Products(ID_product)
 alter table Items_of_orders
 add foreign key (ID_order) references Orders(ID_order)
 alter table Orders
 add foreign key (ID_user) references Users(ID_user)
-
--- Insert data
-insert into dbo.Categories (ID_subcategory, CName)
-	values(NULL, 'Bombs')
-go
-insert into dbo.Categories (ID_subcategory, CName)
-	values(1, 'Bombs with glitter')
-go
-insert into dbo.Products (ID_category, PName, Amount, Cost)
-	values (1, 'Bomb', 5, 150)
-go
-insert into dbo.Products (ID_category, PName, Amount, Cost)
-	values (2, 'Bomb1', 0, 200)
-go
